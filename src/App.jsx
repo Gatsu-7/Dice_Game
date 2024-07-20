39fe64a186702bcfaf9de01274d2387980f2228d
Gatsu-7 <satysat03@gmail.com>
2024-07-20 19:02:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
+// import "./index.css";
+function App() {
+  const [isGameStarted, setisGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setisGameStarted((prev) => !prev);
+  };
+  return (
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
+}
+
+export default App;
